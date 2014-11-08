628f471b3a45c7896d84b5f152dc6e2efabdb1d4
Tom Chen <developer@tomchentw.com>
2014-11-08 17:45:52 +0800

    chore(init): scaffold from react-toastr

---
+++ client/scripts/index.js
@@ -0,0 +1,5 @@
+"use strict";
+require("../styles/index.scss");
+require("prism/themes/prism.css");
+
+var React = require("react/addons");
---
+++ client/webpack.config.js
@@ -0,0 +1,59 @@
+"use strict";
+/**
+ * __template__gist__: https://gist.github.com/tomchentw/368a93bb748ad9d576f1#file-webpack-config-js
+ */
+var Path = require("path"),
+    webpack = require("webpack"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    webpackConfig,
+
+    IS_PRODUCTION = "production" === process.env.NODE_ENV,
+    JSX_LOADER = "jsx-loader?harmony",
+    CSS_LOADER = "style-loader!css-loader?root=../",
+    SCSS_LOADER = CSS_LOADER + "!sass-loader?" + JSON.stringify({
+      includePaths: [
+        Path.resolve(__dirname, "../bower_components/bootstrap-sass-official/assets/stylesheets"),
+      ]
+    });
+
+webpackConfig = module.exports = {
+  entry: "./client/scripts/index.js",
+  output: {
+    path: Path.resolve(__dirname, "../public/assets"),
+    publicPath: "assets/",
+    filename: (IS_PRODUCTION ? "[hash].js" : "bundle.js")
+  },
+  resolve: {
+    root: [
+      Path.join(__dirname, "../bower_components")
+    ]
+  },
+  module: {
+    loaders: [
+      { test: require.resolve("react/addons"), loader: "expose-loader?React" },
+      { test: /\.js(x?)$/, loader: JSX_LOADER },
+      { test: /\.jpg$/, loader: "file-loader" },
+      { test: /\.css$/, loader: CSS_LOADER },
+      { test: /\.scss$/, loader: SCSS_LOADER },
+    ]
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin("bower.json", ["main"])
+    ),
+    new webpack.ProvidePlugin({
+        $: "jquery",
+        jQuery: "jquery"
+    }),
+    new HtmlWebpackPlugin({
+      template: "./client/index.html",
+      filename: "../index.html"
+    })
+  ]
+};
+
+if (IS_PRODUCTION) {
+  webpackConfig.plugins.push(
+    new webpack.optimize.DedupePlugin()
+  );
+}
