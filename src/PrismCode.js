29d6717dc52fc0d430f44af6ca05448fa68642c9
Tom Chen <developer@tomchentw.com>
2014-11-08 22:45:48 +0800

    fix(PrismCode): async option should be truely async
    * see https://github.com/LeaVerou/prism/issues/392

--- src/PrismCode.js
+++ src/PrismCode.js
@@ -22,11 +22,22 @@ module.exports = React.createClass({
   },
 
   componentDidMount () {
-    Prism.highlightElement(this.refs.code.getDOMNode(), this.props.async, this._on_hightlighted);
+    // https://github.com/LeaVerou/prism/issues/392
+    var {_hightlight_element} = this;
+    if (this.props.async) {
+      setTimeout(_hightlight_element);
+    } else {
+      _hightlight_element();
+    }
   },
 
   render () {
     return this._render(this.props, this.state);
+  },
+
+  _hightlight_element () {
+    // https://github.com/LeaVerou/prism/issues/392
+    Prism.highlightElement(this.refs.code.getDOMNode(), this.props.async, this._on_hightlighted);
   },
 
   _on_hightlighted (__html) {
@@ -34,8 +45,15 @@ module.exports = React.createClass({
   },
 
   _render (props, {_hightlighted}) {
-    return <code ref="code" className={props.className} dangerouslySetInnerHTML={_hightlighted}>
-      {_hightlighted ? null : props.children}
-    </code>;
+    var newProps = {
+      ref: "code",
+      className: props.className,
+    }, {children} = props;
+
+    if (_hightlighted) {
+      newProps.dangerouslySetInnerHTML = _hightlighted;
+      children = null;
+    }
+    return <code {...newProps}>{children}</code>;
   }
 });
