2d31414808c9871dde5648684e9f5ed070ad4e7c
Tom Chen <developer@tomchentw.com>
2015-04-27 09:55:40 +0800

    fix(webpack.config.js): node_modules are excluded from js loaders

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,7 +31,7 @@ webpackConfig = module.exports = {
   module: {
     loaders: [
       { test: require.resolve("react/addons"), loader: "expose-loader?React" },
-      { test: /\.js(x?)$/, loaders: JSX_WITH_HOT_LOEADERS },
+      { test: /\.js(x?)$/, exclude: /node_modules/, loaders: JSX_WITH_HOT_LOEADERS },
       { test: /\.jpg$/, loader: "file-loader" },
       { test: /\.css$/, loader: CSS_LOADER },
       { test: /\.scss$/, loader: SCSS_LOADER },
