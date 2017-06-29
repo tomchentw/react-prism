8c34650a1eed441c213583423c7b39f9dad51fd7
Tom Chen <developer@tomchentw.com>
2017-06-30 07:53:58 +0800

    chore: migrate to latest create-react-app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
+/* eslint-disable import/no-webpack-loader-syntax */
 import React from "react";
 
 import ReactDOM from "react-dom";
 
-import rawPrismJs from "raw!prismjs";
+import rawPrismJs from "!raw-loader!prismjs";
 
-import rawSelfUpdatedCode from "!raw!./app/SelfUpdatedCode";
+import rawSelfUpdatedCode from "!raw-loader!./app/SelfUpdatedCode";
 
-import rawPrismText from "raw!./app/SelfUpdatedCode/prism.text";
+import rawPrismText from "!raw-loader!./app/SelfUpdatedCode/prism.text";
 
-import rawMarkupText from "raw!./app/SelfUpdatedCode/markup.text";
+import rawMarkupText from "!raw-loader!./app/SelfUpdatedCode/markup.text";
 
 import App from "./app/App";
 
