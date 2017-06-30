7b7edb07563034cab12fdd49dfbe5bd68b22ee89
Tom Chen <developer@tomchentw.com>
2017-06-30 09:08:09 +0800

    refactor: directory structure for `styleguidist`

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,8 @@
-import { PrismCode } from "./index";
+import PrismCode, { PrismCode as NamedPrismCode } from "./index";
 
 describe(`index`, () => {
   it(`should export components`, () => {
     expect(PrismCode).toBeDefined();
+    expect(NamedPrismCode).toBeDefined();
   });
 });
